f95ac72cda826e696947e697eb4f4637de09a7d7
Brian Hie <brianhie@mit.edu>
2018-10-03 09:13:25 -0400

    some code refactoring

---
+++ bin/pallium.py
@@ -0,0 +1,30 @@
+import numpy as np
+from scanorama import *
+from scipy.sparse import vstack
+
+from process import load_names
+from sketch import reduce_dimensionality, test
+from utils import log
+
+NAMESPACE = 'pallium'
+METHOD = 'svd'
+DIMRED = 100
+
+data_names = [
+    'data/murine_atlases/neuron_1M/neuron_1M',
+]
+
+if __name__ == '__main__':
+    datasets, genes_list, n_cells = load_names(data_names)
+    datasets, genes = merge_datasets(datasets, genes_list)
+    
+    log('Dimension reduction with {}...'.format(METHOD))
+    X = vstack(datasets)
+    X_dimred = reduce_dimensionality(X, method=METHOD, dimred=DIMRED)
+    if METHOD == 'jl_sparse':
+        X_dimred = X_dimred.toarray()
+    log('Dimensionality = {}'.format(X_dimred.shape[1]))
+
+    test(X_dimred, 'pallium', kmeans=False, visualize_orig=False)
+    
+    log('Done.')
